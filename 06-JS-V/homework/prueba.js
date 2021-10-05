6da211e9b6ff370adbd249c987e2ee06a7c1508b
NaniBM <bazanmurua@gmail.com>
2021-10-05 20:18:51 -0300

    homework 06 lista

---
+++ 06-JS-V/homework/prueba.js
@@ -0,0 +1,13 @@
+String.prototype.reverse = function () {
+    var invertida = '';
+    for(var i= this.length - 1; i>=0; i-- ){
+      invertida = invertida + this.charAt(i)
+    }
+    return invertida; 
+  }
+
+
+var cadena = 'hola'
+
+var long = cadena.reverse()
+console.log(long)
